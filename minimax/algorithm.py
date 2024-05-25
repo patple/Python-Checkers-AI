0a0ab987e6a9cb402e5c056a255ea6b095938e3c
patple <patple2003@gmail.com>
2024-05-25 15:11:31 -0700

    new error the takes our move now

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -40,24 +40,24 @@ def simulate_move(piece, move, board, game, skip):
 
 def get_all_moves(board, color, game):
     moves = []
-
+    
     for piece in board.get_all_pieces(color):
         valid_moves = board.get_valid_moves(piece)
         for move, skip in valid_moves.items():
-            draw_moves(game, board, piece)
             temp_board = deepcopy(board)
             temp_piece = temp_board.get_piece(piece.row, piece.col)
             new_board = simulate_move(temp_piece, move, temp_board, game, skip)
-            moves.append(new_board)
-    
+            moves.append([new_board, piece])
+            
     return moves
+    
 
 
-def draw_moves(game, board, piece):
-    valid_moves = board.get_valid_moves(piece)
-    board.draw(game.win)
-    pygame.draw.circle(game.win, (0,255,0), (piece.x, piece.y), 50, 5)
-    game.draw_valid_moves(valid_moves.keys())
-    pygame.display.update()
-    #pygame.time.delay(100)
+# def draw_moves(game, board, piece):
+#     valid_moves = board.get_valid_moves(piece)
+#     board.draw(game.win)
+#     pygame.draw.circle(game.win, (0,255,0), (piece.x, piece.y), 50, 5)
+#     game.draw_valid_moves(valid_moves.keys())
+#     pygame.display.update()
+#     #pygame.time.delay(100)
 
